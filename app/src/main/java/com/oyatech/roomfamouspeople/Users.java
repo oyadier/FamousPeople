bfeb5d8d16cac099342c3fdd7d6eab447dbe4c00
robertmensah <oyadevelop@gmail.com>
2020-09-24 18:28:44 +0000

    Room dependencies set up

--- app/src/main/java/com/oyatech/roomfamouspeople/Users.java
+++ app/src/main/java/com/oyatech/roomfamouspeople/Users.java
@@ -1,9 +1,20 @@
 package com.oyatech.roomfamouspeople;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import com.google.android.material.shape.MaterialShapeDrawable;
+
+@Entity
 public class Users {
+    @PrimaryKey (autoGenerate = true)
     private int id ;
+    @ColumnInfo(name = "first_name")
     private String firstName;
+    @ColumnInfo(name = "second_name")
     private String secondName;
+    @ColumnInfo(name = "email")
     private String email;
 
     public Users(int pId, String pFirstName, String pSecondName, String pEmail) {
@@ -11,6 +22,10 @@ public class Users {
         firstName = pFirstName;
         secondName = pSecondName;
         email = pEmail;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getFirstName() {
